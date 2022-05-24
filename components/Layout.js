a1badfde6789d8c41b7629a8efd8ca08420cce06
Taylor Thornton <thornrae@gmail.com>
2022-05-23 19:52:32 -0700

    added showcase component and use router object to write a conditional rendering statement for showcase component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "./Showcase";
 import styles from "@/styles/Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -11,7 +15,8 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content="{description}" />
         <meta name="keywords" content="{keywords}" />
       </Head>
-      <Header></Header>
+      <Header />
+      {router.pathname === "/" && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer></Footer>
     </div>
