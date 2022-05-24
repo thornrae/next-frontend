9597d966761c93afc9f3c28882e40f1c4f7aeda0
Taylor Thornton <thornrae@gmail.com>
2022-05-23 19:27:14 -0700

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import { FaExclamationTriangle } from "react-icons/fa";
+
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Page not found">
+      <div className={styles.error}>
+        <h4>
+          <FaExclamationTriangle />
+          404
+        </h4>
+        <h4>Sorry, there is nothing here</h4>
+        <Link href="/">Go home</Link>
+      </div>
+    </Layout>
+  );
+}
