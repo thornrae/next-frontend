be0e6cd798195748b74987aba2ce7c4cb54d45cb
Taylor Thornton <thornrae@gmail.com>
2022-05-23 18:44:33 -0700

    reference router object in slug,js

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
 
 export default function EventPage() {
+  const router = useRouter();
+
+  console.log(router);
+
   return (
     <div>
-      <h1>
-        My Event
-      </h1>
+      <h1>My Event</h1>
     </div>
-  )
+  );
 }
