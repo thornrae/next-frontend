b40465dca5ce805cefce004450d4678dce1e9356
Taylor Thornton <thornrae@gmail.com>
2022-05-23 19:16:32 -0700

    header and footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyriht &copy; DJ Events 2022</p>
+      <Link href="/about">About this project</Link>
+    </footer>
+  );
+}
