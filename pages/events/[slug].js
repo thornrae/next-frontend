b4522af142fd0b479b40e5629a72669736172d60
Taylor Thornton <thornrae@gmail.com>
2022-05-23 18:45:24 -0700

    and with button..

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -9,6 +9,8 @@ export default function EventPage() {
   return (
     <div>
       <h1>My Event</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push("/")}>Click</button>
     </div>
   );
 }
