705aa853f0130320936093476c42a26ad369afde
Taylor Thornton <thornrae@gmail.com>
2022-05-23 19:34:24 -0700

    module aliases

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import { FaExclamationTriangle } from "react-icons/fa";
 
-import Layout from "../components/Layout";
-import styles from "../styles/404.module.css";
+import Layout from "@/components/Layout";
+import styles from "@/styles/404.module.css";
 import Link from "next/link";
 
 export default function NotFoundPage() {
