cacc101b8166ca96a31bc4816df1fb24bedb8d7c
Taylor Thornton <thornrae@gmail.com>
2022-05-24 19:30:14 -0700

    using getStaticProps and getStaticPaths to render props

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,47 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   );
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+//using getStaticProps -- you need getStatic paths ^^
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
+}
+
+// using getServerSideProps
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: { evt: events[0] },
+//   };
+// }
