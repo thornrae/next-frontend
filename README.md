eafe58a9f501ac3f0cb6c1245862cc4ee900a78f
Taylor Thornton <thornrae@gmail.com>
2022-05-24 17:20:10 -0700

    getServerSideProps & getStaticProps

---
+++ README.md
@@ -0,0 +1,5 @@
+
+### Server side rendering:
+-getStaticProps (static generation) fetch data at build time
+-getStaticPaths (static generation): specify dynamic routes to -prerender pages based on data
+-getServerSideProps (Server-side rendering): fetch data on request
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  //this console logs on the client
+  //now able to use data provided as props from server
+  //in UI
+  console.log(events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+// export async function getServerSideProps() {
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  // //console log in server
+  // console.log(events);
+
+  //want to pass to client side components,
+  //return events object as props here
+  //pass events object as props to HomePage component
+  return {
+    props: { events },
+    //if not found will make a  new request
+    //solution when  using getStaticProps()
+    //to account for new/changed data
+    revalidate: 1,
+  };
 }
