41f2dccee4080a0da3e80b57467a89986a40c785
Taylor Thornton <thornrae@gmail.com>
2022-05-30 12:45:42 -0700

    created event content type in strapi

--- components/EventItem.js
+++ components/EventItem.js
@@ -7,7 +7,11 @@ export default function EventItem({ evt }) {
     <div className={styles.event}>
       <div className={styles.img}>
         <Image
-          src={evt.img ? evt.img : "/images/event-default.png"}
+          src={
+            evt.image
+              ? evt.image.formats.thumbnail.url
+              : "/images/event-default.png"
+          }
           width={170}
           height={100}
           alt={evt.id}
@@ -16,7 +20,7 @@ export default function EventItem({ evt }) {
 
       <div className={styles.info}>
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString("en-us")} at {evt.time}
         </span>
         <h3>{evt.name}</h3>
       </div>
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -24,12 +24,18 @@ export default function EventPage({ evt }) {
           </a>
         </div>
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString("en-us")}
+          at {evt.time}
         </span>
         <h1>{evt.name}</h1>
         {evt.image && (
           <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} alt={evt.id} />
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+              alt={evt.id}
+            />
           </div>
         )}
         <h3>Performers:</h3>
@@ -48,7 +54,7 @@ export default function EventPage({ evt }) {
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events`);
   const events = await res.json();
 
   const paths = events.map((evt) => ({
@@ -63,7 +69,7 @@ export async function getStaticPaths() {
 
 //using getStaticProps -- you need getStatic paths ^^
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
   const events = await res.json();
 
   return {
