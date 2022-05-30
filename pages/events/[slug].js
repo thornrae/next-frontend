8a16c3ebd57a09e77d72f06e4911a916ca3eccae
Taylor Thornton <thornrae@gmail.com>
2022-05-30 15:27:57 -0700

    delete event

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,14 +1,31 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import styles from "@/styles/Event.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import { FaEject, FaPencilAlt, FaTimes } from "react-icons/fa";
 import { FetchEvent } from "next/dist/server/web/spec-compliant/fetch-event";
+import { useRouter } from "next/router";
 
 export default function EventPage({ evt }) {
-  const deleteEvent = (e) => {
-    console.log("delete");
+  const router = useRouter();
+
+  const deleteEvent = async (e) => {
+    //cnonfirmation pop up
+    if (confirm("are you sure?")) {
+      const res = await fetch(`${API_URL}/events/${evt.id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        toast.error(data.message);
+      } else {
+        //redirects to this route
+        router.push("/events");
+      }
+    }
   };
   return (
     <Layout>
@@ -20,7 +37,7 @@ export default function EventPage({ evt }) {
             </a>
           </Link>
           <a href="#" className={styles.delete} onClick={deleteEvent}>
-            <FaTimes />
+            <FaTimes /> Delete Event
           </a>
         </div>
         <span>
@@ -28,6 +45,7 @@ export default function EventPage({ evt }) {
           at {evt.time}
         </span>
         <h1>{evt.name}</h1>
+        <ToastContainer />
         {evt.image && (
           <div className={styles.image}>
             <Image
