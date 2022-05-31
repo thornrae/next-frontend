52028da21f97e17b655f333f651f7f0a19dbeb0f
Taylor Thornton <thornrae@gmail.com>
2022-05-30 20:42:54 -0700

    reusable modal component using createPortal and _document.js in /pages

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
-
+import Modal from "@/components/Modal";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Form.module.css";
 
@@ -25,6 +25,8 @@ export default function EditEventPage({ evt }) {
   const [imagePreview, setImagePreview] = useState(
     evt.image ? evt.image.formats.thumbnail.url : null
   );
+
+  const [showModal, setShowModal] = useState(false);
 
   const router = useRouter();
 
@@ -163,10 +165,14 @@ export default function EditEventPage({ evt }) {
       )}
 
       <div>
-        <button className="btn-secondary">
+        <button onClick={() => setShowModal(true)} className="btn-secondary">
           <FaImage /> Set Image
         </button>
       </div>
+
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        IMAGE UPLOAD
+      </Modal>
     </Layout>
   );
 }
