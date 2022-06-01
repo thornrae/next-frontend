9a0b1c40c37922dc107fc6ea2a2360e75649a073
Taylor Thornton <thornrae@gmail.com>
2022-05-31 19:16:55 -0700

    pagination without component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,19 @@
 import Layout from "@/components/Layout";
 import EventItem from "@/components/EventItem";
-
+import Link from "next/link";
 import { API_URL } from "@/config/index";
 
-export default function EventsPage({ events }) {
+const PER_PAGE = 5;
+
+export default function EventsPage({ events, page, total }) {
   //this console logs on the client
   //now able to use data provided as props from server
   //in UI
-  console.log(events);
+  // console.log(events);
+
+  //need to figure out last page
+  const lastPage = Math.ceil(total / PER_PAGE);
+
   return (
     <Layout>
       <h1>Events</h1>
@@ -16,17 +22,44 @@ export default function EventsPage({ events }) {
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+
+      {page > 1 && (
+        <Link href={`/events?page=${page - 1}`}>
+          <a className="btn-secondary">Prev</a>
+        </Link>
+      )}
+
+      {page < lastPage && (
+        <Link href={`/events?page=${page + 1}`}>
+          <a className="btn-secondary">Next</a>
+        </Link>
+      )}
     </Layout>
   );
 }
 
-// export async function getServerSideProps() {
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-  const events = await res.json();
+export async function getServerSideProps({ query: { page = 1 } }) {
+  //destructuring query and getting page
+  // export async function getStaticProps() {
+  // console.log("pg", page);
+
+  //calculate start page
+  //+ same as parseInt
+  //with thise code, if url is: /events?page=2 -- it will show
+  //events on second page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
+  //need to fetch total number events to pass as a prop into our component
+  //in the return body
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  const total = await totalRes.json();
+
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
+  const events = await eventRes.json();
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   };
 }
